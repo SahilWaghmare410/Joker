0ae2dd7a342f16d0a8395e367f19a5258d90bba4
Sahil Waghmare <sahilwaghmare410@gmail.com>
2022-06-08 19:12:57 +0530

    Added side bar

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const url = "https://api.chucknorris.io/jokes/random";
+const url2 = "https://api.chucknorris.io/jokes/categories";
 
 const btnJoke = document.querySelector(".btns");
 const closeBtn = document.querySelector(".close-modal");
 
 const modalWindow = document.querySelector(".modal");
 const ApiData = document.querySelector(".addAPIData");
+
+//SIDEBAR
+const sidebarclosebtn = document.querySelector(".closeSideBtn");
+const sideBar = document.querySelector(".sideBarClass");
+const sideBarOpen = document.querySelector(".openSideBar");
+const ApiDataCate = document.querySelector(".contentCate");
 
 // console.log("b");
 let joke;
@@ -15,6 +22,7 @@ function apiCall() {
       return response.json();
     })
     .then((data) => {
+      console.log(data);
       joke = data.value;
       ApiData.textContent = joke;
       console.log(joke);
@@ -23,6 +31,31 @@ function apiCall() {
       console.log("Error detected");
     });
 }
+
+function apiCallCategories() {
+  fetch(url2)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      let conte = "";
+      console.log(data.length);
+      for (let i = 0; i < data.length; i++) {
+        conte += data[i] + "\n";
+        ApiDataCate.textContent += "${data[i]} \n";
+        // System.lineSeparator();
+      }
+      console.log(conte);
+      ApiDataCate.textContent = conte;
+
+      console.log(data);
+    })
+    .catch(function () {
+      console.log("Error detected");
+    });
+}
+apiCallCategories();
 apiCall();
 //new approach
 btnJoke.addEventListener("click", function () {
@@ -32,4 +65,16 @@ btnJoke.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
   apiCall();
   modalWindow.classList.add("hidden");
+});
+
+//sidebarclass btn open
+sideBarOpen.addEventListener("click", function () {
+  sideBar.classList.remove("hidden");
+  sideBarOpen.classList.add("hidden");
+});
+
+//sidebarclass btn close
+sidebarclosebtn.addEventListener("click", function () {
+  sideBar.classList.add("hidden");
+  sideBarOpen.classList.remove("hidden");
 });
